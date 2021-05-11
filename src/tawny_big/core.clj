ea5058135a4c65f75ee77f3313aa8823a229e198
Jennifer Warrender <jaydchan@gmail.com>
2021-05-11 17:26:33 +0100

    Add tawny.owl dependency and create example ontologies of size 5

--- src/tawny_big/core.clj
+++ src/tawny_big/core.clj
@@ -1,7 +1,32 @@
 (ns tawny-big.core
+  (:use [tawny.owl])
   (:gen-class))
+
+(defn create-big
+  "Create a big ontology"
+  [size]
+
+  ;; create ontology
+  (defontology example
+    :iri "https://www.example.com/"
+    :noname true)
+
+  ;; for x in 0..size, create and add class
+  (dotimes [x size]
+    (owl-class-explicit example (str (+ x 1)) ()))
+
+  ;; println ontology
+  (println example)
+  (println (owl-class "1"))
+  (println example)
+
+  ;; save ontology in .omn and .owl format
+  (save-ontology example "example.omn" :omn)
+  (save-ontology example "example.owl" :owl))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ;; create ontology of size=5
+  (create-big 5))
+
